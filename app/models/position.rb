9ecd9ce58627267357b90e8e6cd7b2d5bc94a8ca
LeXuankhanh22070537 <Xkhanhle228@gmail.com>
2024-05-15 11:00:34 +0700

    Add model validation and association

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,2 +1,6 @@
 class Position < ApplicationRecord
+    belongs_to :book
+    validates_presence_of :book, :floor, :bookshelves
+    validates_numericality_of :row
+
 end
--- app/models/writer.rb
+++ app/models/writer.rb
@@ -1,2 +1,7 @@
 class Writer < ApplicationRecord
+    has_many :books
+    validates_presence_of :name
+    validates_uniqueness_of :name
+    validates_numericality_of :year
+
 end
