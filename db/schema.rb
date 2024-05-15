c7840fc0358df6961fada0701088eff13fced630
LeXuankhanh22070537 <Xkhanhle228@gmail.com>
2024-05-15 09:16:21 +0700

    psql

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_24_112825) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "DoB"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
