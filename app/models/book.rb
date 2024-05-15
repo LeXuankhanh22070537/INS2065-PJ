7dc3ab8ed785a656f0b4f70c231ea33e6edf3a94
LeXuankhanh22070537 <Xkhanhle228@gmail.com>
2024-05-15 20:57:08 +0700

    Add model validation and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :subject
+  has_many :writers
+  belongs_to :position
 
   validates_presence_of :title, :author, :publisher
   validates_uniqueness_of :title
