edd1a4f0b456343bc424892112d43b7e6a6686ea
LeXuankhanh22070537 <Xkhanhle228@gmail.com>
2024-05-16 16:49:54 +0700

    Change logo and fix code

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :subject
   has_many :writers
   belongs_to :position
-  has_many: genres
+  has_many:genres
 
   validates_presence_of :title, :author, :publisher
   validates_uniqueness_of :title
